d4c1a46a252f05ed0392ce94990bae5ec66f545a
Haiol <lovehp30@gmail.com>
2021-11-03 20:40:16 +0900

    add Fragment Nav And Draw The seekbar.xml

--- app/src/main/java/com/lovehp30/mirrorcontrol/Test2Fragment.java
+++ app/src/main/java/com/lovehp30/mirrorcontrol/Test2Fragment.java
@@ -1,25 +1,35 @@
 package com.lovehp30.mirrorcontrol;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class TestFragment extends Fragment {
+public class Test2Fragment extends Fragment {
+
+
+    public static Test2Fragment newInstance(String param1, String param2) {
+        Test2Fragment fragment = new Test2Fragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
 
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return inflater.inflate(R.layout.fragment_test2, container, false);
     }
 }
