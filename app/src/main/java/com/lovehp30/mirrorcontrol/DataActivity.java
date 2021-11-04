bd19d1a384494700247bb43e330d60540f02c465
Haiol <lovehp30@gmail.com>
2021-11-04 20:09:56 +0900

    add tabLayout, modify ActViewAdapter

--- app/src/main/java/com/lovehp30/mirrorcontrol/DataActivity.java
+++ app/src/main/java/com/lovehp30/mirrorcontrol/DataActivity.java
@@ -1,5 +1,6 @@
 package com.lovehp30.mirrorcontrol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
@@ -8,6 +9,7 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +17,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,7 +31,6 @@ public class DataActivity extends AppCompatActivity {
     String Client_code = "TestWemos01";
     String Topics = "Sensor";
     public static String ip ="lovehp12.duckdns.org";;
-    RequestData requestData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +39,9 @@ public class DataActivity extends AppCompatActivity {
         TextView subtitle = findViewById(R.id.dt_subtitle);
         subtitle.setText(Topics);
         toolbar.setTitle(Client_code);
-        DataSphereFragment.putID(Client_code,Topics);
         setSupportActionBar(toolbar);
         ViewPager2 pager = findViewById(R.id.dt_viewpager);
+        TabLayout tableLayout = findViewById(R.id.dt_tlTabs);
         // ToolBar Setting
         List<SensorDataFormat> list = new ArrayList<>();
         final ProgressDialog myProgressDialog = ProgressDialog.show(this, "Gathering the Your Data", "please Wait..", true);
@@ -52,12 +55,27 @@ public class DataActivity extends AppCompatActivity {
                     try{
                         JSONArray array = new JSONArray(response);
                         for(int i=0;i<array.length();i++){
-
+                            JSONObject object = array.getJSONObject(i);
+                            list.add(new SensorDataFormat(object.getString("clientID"),object.getString("message"),object.getString("time")));
                         }
 
                     }catch (JSONException e){
                         e.printStackTrace();
                     }
+                    //Json 분석
+                    DataActViewAdapter adapter = new DataActViewAdapter(Client_code,Topics,list,getSupportFragmentManager(), getLifecycle());
+                    pager.setAdapter(adapter);
+                    String map[] = list.get(0).getMapKeyList();
+
+                    new TabLayoutMediator(tableLayout, pager, new TabLayoutMediator.TabConfigurationStrategy() {
+                        @Override
+                        public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                                if(position==0)
+                                    tab.setText("DataStream");
+                                else
+                                    tab.setText(map[position-1]);
+                        }
+                    }).attach();
 
                     myProgressDialog.dismiss();
                 },
@@ -70,9 +88,6 @@ public class DataActivity extends AppCompatActivity {
         );
         queue.add(request);
 
-        //Json 분석
-        DataActViewAdapter adapter = new DataActViewAdapter(Client_code,Topics,getSupportFragmentManager(), getLifecycle());
-        pager.setAdapter(adapter);
 
     }
 
