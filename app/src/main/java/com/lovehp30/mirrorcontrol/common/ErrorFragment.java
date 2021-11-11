67b83e491a52b6256a2a94ac4fbdc9a32d19b523
Haiol <lovehp30@gmail.com>
2021-11-11 20:52:07 +0900

    new version1

--- app/src/main/java/com/lovehp30/mirrorcontrol/common/ErrorFragment.java
+++ app/src/main/java/com/lovehp30/mirrorcontrol/common/ErrorFragment.java
@@ -55,6 +55,7 @@ public class ErrorFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            /****/
         }
     }
 
